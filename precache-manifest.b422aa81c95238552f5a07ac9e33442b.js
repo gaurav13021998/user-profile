59e6f803251a4dec6c833bd676db54d31ceeeead
Gaurav singh <gaurav13021997@gmail.com>
2021-07-10 10:56:03 +0530

    Updates

---
+++ precache-manifest.b422aa81c95238552f5a07ac9e33442b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb0295d2dfde3c3aa0d517eb40bacce",
+    "url": "/user-profile/index.html"
+  },
+  {
+    "revision": "a9cb647192c314325c59",
+    "url": "/user-profile/static/css/2.7118e3f5.chunk.css"
+  },
+  {
+    "revision": "984484ea5305686257c9",
+    "url": "/user-profile/static/css/main.165db444.chunk.css"
+  },
+  {
+    "revision": "a9cb647192c314325c59",
+    "url": "/user-profile/static/js/2.24acaf07.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/user-profile/static/js/2.24acaf07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984484ea5305686257c9",
+    "url": "/user-profile/static/js/main.c43997cc.chunk.js"
+  },
+  {
+    "revision": "1573654686d9410621ed",
+    "url": "/user-profile/static/js/runtime-main.4ebade16.js"
+  }
+]);
